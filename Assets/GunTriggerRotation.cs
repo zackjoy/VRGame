1992280ee1c18fc14fc6b20fb0a3401296558ee7
BWillis98 <bwmusiclover1212@gmail.com>
2018-04-13 23:46:46 -0500

    Initial upload of entire project

---
+++ Assets/GunTriggerRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTriggerRotation : MonoBehaviour
+{
+    public GameObject controllerRight;
+    public GameObject controllerLeft;
+    public float triggerPressed;
+    private SteamVR_Controller.Device device;
+    private Trigger controller;
+    private float minTriggerRotation = -10f;
+    private float maxTriggerRotation = 45f;
+
+
+    void Update ()
+    {
+        controller = controllerRight.GetComponent<Trigger>();
+        controller.triggerAxis += triggerPressed;
+        controller = controllerLeft.GetComponent<Trigger>();
+        controller.triggerAxis += triggerPressed;
+        Debug.Log("TriggerPressed = " + triggerPressed);
+
+    }
+}
---
+++ Assets/flashlight/Flashlight.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using VRTK;
+using VRTK.GrabAttachMechanics;
+
+public class Flashlight : MonoBehaviour
+{
+    public VRTK_InteractableObject interactableObject;
+    public VRTK_FixedJointGrabAttach fixedJoint;
+    public Light flashLight;
+    public Transform snapHandle;
+    private bool isActive;
+    private bool isForward;
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+    public GameObject controllerRight;
+    private SteamVR_TrackedController controller;
+    // Use this for initialization
+    void Start()
+    {
+        isActive = true;
+        isForward = true;
+        controller = controllerRight.GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += TriggerPressed;
+        controller.MenuButtonClicked += MenuPressed;
+        trackedObj = controllerRight.GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void TriggerPressed(object sender, ClickedEventArgs e)
+    {
+        if (interactableObject.enabled == true)
+            TurnOff();
+    }
+
+    private void MenuPressed(object sender, ClickedEventArgs e)
+    {
+        if (interactableObject.enabled == true)
+            ChangeRotation();
+    }
+
+    public void TurnOff()
+    {
+        if (isActive == false)
+        {
+            flashLight.enabled = true;
+            isActive = true;
+        }
+        else if (isActive == true)
+        {
+            flashLight.enabled = false;
+            isActive = false;
+        }
+    }
+
+    public void ChangeRotation()
+    {
+        if (isForward == false)
+        {
+            fixedJoint.rightSnapHandle.GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            fixedJoint.leftSnapHandle.GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            isForward = true;
+        }
+        else if (isForward == true);
+        {
+            fixedJoint.rightSnapHandle.GetComponent<Transform>().eulerAngles = new Vector3(180, 0, 0);
+            fixedJoint.leftSnapHandle.GetComponent<Transform>().eulerAngles = new Vector3(180, 0, 0);
+            isForward = false;
+        }
+    }
+
+}
+
+
+
